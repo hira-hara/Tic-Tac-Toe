d1cccefa516ebe074381db9ba91f4b55c9c3500d
Ida_Su <idasuozdemir@gmail.com>
2024-09-14 13:14:07 -0400

    Added board function

--- src/board.cpp
+++ src/board.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 void boardDisplay(char board[3][3]) {
     cout << "-------------------------\n";
-    for (int j=0; j < 3; j++) {
-        for (int i=0; i < 4; i++) {
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
             cout << board[j][i] << "|\t";
         }
+        cout << "|";
         cout << "\n";
         cout << "-------------------------\n";
     }
 }
+
 
 int main(){
     char board[3][3] = { { ' ', ' ', ' ' }, 
